20abab532b0a4f2b43f0386b528231a98584ce33
eko24 <eko24ive@gmail.com>
2017-02-03 15:00:25 +0200

    BUILD 0.0.1

---
+++ _site/js/main.js
@@ -0,0 +1,28 @@
+$(window).load(function() {
+  $("#preloader").fadeOut("normal");
+});
+
+$(document).ready(function(){
+   Gifffer({
+     playButtonStyles: {
+       'width': '60px',
+       'height': '60px',
+       'border-radius': '30px',
+       'background': 'rgba(0, 0, 0, 0.3)',
+       'position': 'absolute',
+       'top': '50%',
+       'left': '50%',
+       'margin': '-30px 0 0 -30px'
+     },
+     playButtonIconStyles: {
+       'width': '0',
+       'height': '0',
+       'border-top': '14px solid transparent',
+       'border-bottom': '14px solid transparent',
+       'border-left': '14px solid rgba(255, 255, 255, 0.5)',
+       'position': 'absolute',
+       'left': '26px',
+       'top': '16px'
+     }
+   });
+});
---
+++ _site/js/search.js
@@ -0,0 +1,76 @@
+(function() {
+  var siteURL = window.location.origin;
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
+
+    if (results.length) { // Are there any results?
+      var appendString = '';
+
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        item.image = JSON.parse(item.image);
+          appendString += ''+
+          '<article class="tile" itemscope itemtype="http://schema.org/Article">'+
+            '<a href="' + item.url + '" title="' + item.title + '" class="post-teaser">';
+            if(item.image !== false && item.image !== undefined) {
+              if(item.image.teaser) {
+                appendString += '<img src="'+ siteURL +'/images/'+item.image.teaser+'" alt="teaser" itemprop="image">';
+              }
+            }
+            appendString += '</a>';
+            if(item.date) {
+              appendString +='<p class="entry-date date published"><time datetime="'+ item.date +'" itemprop="datePublished">'+ item.date +'</time></p>';
+            }
+          appendString += '<h2 class="post-title" itemprop="name"><a href="' + item.url + '">' + item.title + '</a></h2>'+
+            '<p class="post-excerpt" itemprop="description">' + item.content.substring(0, 150) + '...</p>'+
+          '</article>';
+      }
+
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
+
+  var searchTerm = getQueryVariable('query');
+
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('author');
+      this.field('category');
+      this.field('content');
+    });
+
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'author': window.store[key].author,
+        'category': window.store[key].category,
+        'content': window.store[key].content
+      });
+
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store); // We'll write this in the next section
+    }
+  }
+})();
